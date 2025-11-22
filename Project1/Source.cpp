c180009aec677c4d4e187562fc1188f47c300049
eddieromero777 <eddie.romero777@icloud.com>
2025-11-21 19:47:17 -0600

    Update Source.cpp

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,9 +1,23 @@
 #include <iostream>
-
+#include <fstream>
+#include <vector>
+#include <string>
 using namespace std;
 
-int main() {
-	cout << "Hello World" << endl;
-
+int main()
+{
+	ifstream dictionaryfile("dictionary.txt");
+	if (dictionaryfile.fail())
+	{
+		cout << "Error: can't open dictionary file." << endl;
+		return 1;
+	}
+	vector<string> dictionary;
+	string word;
+	while (dictionaryfile >> word)
+	{
+		dictionary.push_back(word);
+	}
+	dictionaryfile.close();
 	return 0;
 }

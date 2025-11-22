79bfd473d5b38a423dd5129c5f17972fa12b8694
eddieromero777 <eddie.romero777@icloud.com>
2025-11-22 12:38:46 -0600

    updated

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main()
 {
-	ifstream dictionaryfile("dictionary.txt");
+	ifstream dictionaryfile("dictionary.txt");//opens dictionary file
 	if (dictionaryfile.fail())
 	{
 		cout << "Error: can't open dictionary file." << endl;
@@ -16,8 +16,35 @@ int main()
 	string word;
 	while (dictionaryfile >> word)
 	{
-		dictionary.push_back(word);
+		dictionary.push_back(word);// puts words vector
 	}
 	dictionaryfile.close();
+
+	ifstream inputfile("input.txt");//opens file with input words
+		if (inputfile.fail())
+		{
+			cout << "Error: can't open input file" << endl;
+			return 2;
+		}
+	cout << "misspelled words:" << endl;
+	while (inputfile >> word)
+	{
+		bool found = false;
+		for (int i = 0; i < dictionary.size(); i++)// loops thru words
+		{
+			if (word == dictionary[i])// checking if same
+			{
+				found = true;//if true means its correct
+			}
+		}
+		if (found == false)// if false means its misspelled
+		{
+			cout << word << endl; // this prints misspelled word
+		}
+	} 
+
+
+
+
 	return 0;
 }
